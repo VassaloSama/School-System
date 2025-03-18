562d764d6404154a3c41139c4cb958c458a36657
Lucas Vassalo <lv.cristiano@hotmail.com>
2025-03-17 23:27:25 -0300

    feat: criando classe aluno

--- app.py
+++ app.py
@@ -160,3 +160,31 @@ def deletar_turma(id):
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
 
+#### ROTA ALUNOS ####
+class Aluno:
+    def __init__(self, id, nome, idade, turma_id, data_nascimento, nota_primeiro_semestre, nota_segundo_semestre):
+        if turma_id not in turmas:
+            raise ValueError("Turma não encontrada!")
+
+        self.id = id
+        self.nome = nome
+        self.idade = idade
+        self.turma_id = turma_id
+        self.data_nascimento = data_nascimento
+        self.nota_primeiro_semestre = float(nota_primeiro_semestre)
+        self.nota_segundo_semestre = float(nota_segundo_semestre)
+        self.media_final = (self.nota_primeiro_semestre + self.nota_segundo_semestre) / 2
+
+        alunos[id] = {
+            "id": self.id,
+            "nome": self.nome,
+            "idade": self.idade,
+            "turma_id": self.turma_id,
+            "data_nascimento": self.data_nascimento,
+            "nota_primeiro_semestre": self.nota_primeiro_semestre,
+            "nota_segundo_semestre": self.nota_segundo_semestre,
+            "media_final": self.media_final
+        }
+    
+    def serialize(self):
+        return alunos[self.id]
