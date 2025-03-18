f24649da3b09756606dfbfc614e9c67a07b671d3
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-17 22:51:16 -0300

    feet: criando classe para entidade professores

--- app.py
+++ app.py
@@ -8,6 +8,27 @@ alunos = {}
 turmas = {}
 professores = {}
 
+#### ROTA PROFESSORES ####
+class Professores:
+    def __init__(self, id, nome, idade, materia):
+        self.id = id
+        self.nome = nome
+        self.idade = idade
+        self.materia = materia
+
+        professores[id] = {
+            "id": self.id,
+            "nome": self.nome,
+            "idade": self.idade,
+            "materia": self.materia
+        }
+    
+    def serialize(self):
+        return professores[self.id]
+    
+
+
 # Rodar o servidor
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
