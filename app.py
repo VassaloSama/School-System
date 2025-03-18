7290364b1d5e99748e0e8be3d59636a4ea3075cc
Luiz Canuto <luizmariocanutto123@gmail.com>
2025-03-17 23:07:51 -0300

    feat: rota de turmas criadas

--- app.py
+++ app.py
@@ -76,6 +76,27 @@ def deletar_professor(id):
     del professores[id]
     return jsonify({"mensagem": "Professor deletado com sucesso!"}), 200
 
+#### ROTA TURMAS ####
+class Turmas:
+    def __init__(self, id, descricao, professor_id, ativo):
+        if professor_id not in professores:
+            raise ValueError("Professor não encontrado!")
+
+        self.id = id
+        self.descricao = descricao
+        self.professor_id = professor_id
+        self.ativo = ativo
+
+        turmas[id] = {
+            "id": self.id,
+            "descricao": self.descricao,
+            "professor_id": self.professor_id,
+            "ativo": self.ativo
+        }
+    
+    def serialize(self):
+        return turmas[self.id]
+
 
 
 
