80ebd4e5407ae272339c2f3ca8d4b28109be6401
Luiz Mario da Silva Canuto <2400464@salas.aulas>
2025-03-19 19:07:59 -0300

    feat: criei os ultimos testes, arrumei erros no PUT

--- app.py
+++ app.py
@@ -155,23 +155,29 @@ def obter_turma(id):
 # PUT TURMAS
 @app.route('/turmas/<int:id>', methods=['PUT'])
 def atualizar_turma(id):
+    dados = request.json
+
     if id not in turmas:
         return jsonify({"erro": "Turma não encontrada!"}), 404
+    print("teste")
     
     if not any(campo in dados for campo in ["descricao", "professor_id", "ativo"]):
         return jsonify({"erro": "Dados Inválidos!"}), 400
     
-    dados = request.json
     professor_id = dados.get("professor_id", turmas[id]["professor_id"])
 
     if professor_id not in professores:
         return jsonify({"erro": "Professor não encontrado!"}), 404
+    
+    print("teste2")
 
-    turmas[id].update({
-        "descricao": dados.get("descricao", turmas[id]["descricao"]),
-        "professor_id": professor_id,
-        "ativo": dados.get("ativo", turmas[id]["ativo"])
-    })
+    if "descricao" in dados:
+        turmas[id]["descricao"] = dados["descricao"]
+    if "professor_id" in dados:
+        turmas[id]["professor_id"] = dados["professor_id"]
+    if "ativo" in dados:
+        turmas[id]["ativo"] = dados["ativo"]
+
     
     return jsonify({"message": "Turma atualizada com sucesso"}), 200
 
