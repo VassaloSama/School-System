26ee3d870c40b340bd556cc9dea15958c0b5031e
Lucas Vassalo Cristiano <2400379@SALAS.AULAS>
2025-04-23 21:56:49 -0300

    feat: configuração do SQLAlchemy no app

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['HOST'] = '0.0.0.0'
+app.config['PORT'] = 5000
+app.config['DEBUG'] = True
+
+# Config Banco MySQL
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://admin:SenhaForte123@localhost:5080/school-system"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
