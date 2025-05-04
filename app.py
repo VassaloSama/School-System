51f30416e13a6b7ab302c4a4141e8e9aa7f83ff8
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-05-04 11:43:47 -0300

    feat: Adicionando o uso do SQLAchemy para a inicialização do app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
 
-from config import app
+
+from config import app, db
 from controller.professor import professoresApp
 from controller.turma import turmasApp
 from controller.aluno import alunosApp
@@ -10,16 +11,21 @@ app.register_blueprint(professoresApp)
 app.register_blueprint(turmasApp)
 app.register_blueprint(alunosApp)
 
+# Criar tabelas no banco
+with app.app_context():
+    db.create_all()
+
 #### ROTA RESETAR DADOS ####
 @app.route('/resetar', methods=['POST'])
 def resetar_dados():
-    from models.professores import professores
-    from models.turmas import turmas
-    from models.alunos import alunos
+    from models.professores import Professor
+    from models.turmas import Turma
+    from models.alunos import Aluno
     
-    alunos.clear()
-    turmas.clear()
-    professores.clear()
+    Aluno.query.delete()
+    Turma.query.delete()
+    Professor.query.delete()
+    db.session.commit()
 
     return jsonify({"mensagem": "Dados resetados com sucesso!"}), 200
     
